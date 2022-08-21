72dc8a449b1728d975db875c5a2d6e74b1db1a64
JacquelynSH <elpatchaembroidery@gmail.com>
2022-08-21 13:11:52 -0700

    Add dashboard w/ CSS. Other small CSS changes made

---
+++ Dashboard_files/app.js
@@ -0,0 +1,11 @@
+// Client facing scripts here
+let map;
+
+      function initMap() {
+        map = new google.maps.Map(document.getElementById("map"), {
+          center: { lat: -34.397, lng: 150.644 },
+          zoom: 8,
+        });
+      }
+
+      window.initMap = initMap;
