7299eeb06bd49bb11ea189fe4057b147444e1279
JacquelynSH <elpatchaembroidery@gmail.com>
2022-08-23 14:47:01 -0700

    Add city search feature that re-centers map

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -10,32 +10,71 @@
 
 //       window.initMap = initMap;
 
+
+// re jigged the code based on a youtube tutorials, same same but different lol
+
 function initMap() {
+  let map;
   let location = {
     lat: -34.397,
-    lng: 150.644
-  }
+    lng: 150.644,
+  };
   let options = {
     center: location,
-    zoom: 8
-  }
+    zoom: 8,
+  };
 
   if (navigator.geolocation) {
-    console.log('Geolocation is here!');
-    navigator.geolocation.getCurrentPosition((loc) => {
-      location.lat = loc.coords.latitude;
-      location.lng = loc.coords.longitude;
-      console.log('USER LOCATION:', location.lat, location.lng)
+    console.log("Geolocation is here!");
+    navigator.geolocation.getCurrentPosition(
+      (loc) => {
+        location.lat = loc.coords.latitude;
+        location.lng = loc.coords.longitude;
+        console.log("USER LOCATION:", location.lat, location.lng);
 
-      map = new google.maps.Map(document.getElementById("map"), options);
-    },
-    (err) => {
-      console.log("User declined locaiton tracking");
-      map = new google.maps.Map(document.getElementById("map"), options);
-    }
-    )
+        map = new google.maps.Map(document.getElementById("map"), options);
+      },
+      (err) => {
+        console.log("User declined locaiton tracking");
+        map = new google.maps.Map(document.getElementById("map"), options);
+      }
+    );
   } else {
-    console.log('No geolocation :(');
+    console.log("No geolocation :(");
     map = new google.maps.Map(document.getElementById("map"), options);
   }
+
+  // adding auto complete to search bar
+
+  autocomplete = new google.maps.places.Autocomplete(
+    document.getElementById("search-input"),
+    {
+      componentRestrictions: { country: ["ca"] },
+      fields: [],
+      types: [],
+    }
+  );
+
+  autocomplete.addListener("place_changed", () => {
+    const place = autocomplete.getPlace();
+    // something here to re-focus the map to a new city
+
+    const lat = place.geometry.location.lat();
+    const lng = place.geometry.location.lng();
+    const center = new google.maps.LatLng(lat, lng);
+    // const map = document.getElementById("map");
+    console.log(map)
+    map.panTo(center);
+    // map = new google.maps.Map(document.getElementById("map"), p);
+  });
+
+  // autocomplete.addListener("place_changed", () => {
+  //   const place = autocomplete.getPlace();
+  //   new google.maps.Marker({
+  //     position: place.geometry.location,
+  //     title: place.name,
+  //     map: map
+
+  //   })
+  // });
 }
