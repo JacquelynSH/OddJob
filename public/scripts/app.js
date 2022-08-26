6854d1b7f3b7162d152d40cad6b256b434fc7124
JacquelynSH <elpatchaembroidery@gmail.com>
2022-08-25 19:08:42 -0700

    Add oddjobs pins, finally :')

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,19 +4,6 @@
 let map;
 let pinsArray;
 
-$(() => {
-  $.ajax("/pins").then(pins => {
-    // pinsArray = pins;
-    for (const pin of pins) {
-      const marker = new google.maps.Marker({
-        position: new google.maps.LatLng(pin.lat, pin.lng),
-        map: map
-      });
-    }
-  })
-})
-
-
 function initMap() {
   //default location
   let location = {
@@ -30,7 +17,7 @@ function initMap() {
     zoom: 10,
   };
 
-  //find if user will allow location tracking
+  // find if user will allow location tracking
   if (navigator.geolocation) {
     console.log("Geolocation is here!");
     navigator.geolocation.getCurrentPosition(
@@ -41,8 +28,10 @@ function initMap() {
         location.lng = loc.coords.longitude;
         console.log("USER LOCATION:", location.lat, location.lng);
 
+
         //draw to map with the target container and the options from above
         map = new google.maps.Map(document.getElementById("map"), options);
+        mapDelay();
       },
       //user location request DENIED!
       (err) => {
@@ -50,61 +39,74 @@ function initMap() {
 
         //draw to map with the target container and the options from above
         map = new google.maps.Map(document.getElementById("map"), options);
+        mapDelay();
       }
     );
   } else {
     console.log("No geolocation :(");
     map = new google.maps.Map(document.getElementById("map"), options);
+    mapDelay();
   }
 
   //autocomplete for map search
-  mapsearch = new google.maps.places.Autocomplete(
-    document.getElementById("search-input"),
-    {
-      componentRestrictions: { country: ["ca"] },
-      fields: ["geometry"],
-      types: [],
-    }
-  );
 
-  //event listener to repostion map on search bar coords
-  mapsearch.addListener("place_changed", () => {
-    const place = mapsearch.getPlace();
-
-    const lat = place.geometry.location.lat();
-    const lng = place.geometry.location.lng();
-    console.log("LAT:", lat, "LNG:", lng);
-
-    const center = {
-      lat,
-      lng
-    }
-    map.panTo(center);
-  });
-
-  //autocomplete for oddjob location
-  joblocation = new google.maps.places.Autocomplete(
-    document.getElementById("location"),
-    {
-      componentRestrictions: { country: ["ca"] },
+  const mapDelay = () => {
+    mapsearch = new google.maps.places.Autocomplete(
+      document.getElementById("search-input"),
+      {
+        componentRestrictions: { country: ["ca"] },
         fields: ["geometry"],
         types: [],
       }
-      );
+    );
 
-  //event listener to console log oddjob location coords
-  joblocation.addListener("place_changed", () => {
-    const place = joblocation.getPlace();
+    //event listener to repostion map on search bar coords
+    mapsearch.addListener("place_changed", () => {
+      const place = mapsearch.getPlace();
 
-    const lat = place.geometry.location.lat();
-    const lng = place.geometry.location.lng();
-    console.log("LAT:", lat, "LNG:", lng);
+      const lat = place.geometry.location.lat();
+      const lng = place.geometry.location.lng();
+      console.log("LAT:", lat, "LNG:", lng);
 
-    document.getElementById('latitude').value = lat;
-    document.getElementById('longitude').value = lng;
-  });
+      const center = {
+        lat,
+        lng,
+      };
+      map.panTo(center);
+    });
 
+    //autocomplete for oddjob location
+    joblocation = new google.maps.places.Autocomplete(
+      document.getElementById("location"),
+      {
+        componentRestrictions: { country: ["ca"] },
+        fields: ["geometry"],
+        types: [],
+      }
+    );
 
-  window.initMap = initMap;
+    //event listener to console log oddjob location coords
+    joblocation.addListener("place_changed", () => {
+      const place = joblocation.getPlace();
+
+      const lat = place.geometry.location.lat();
+      const lng = place.geometry.location.lng();
+      console.log("LAT:", lat, "LNG:", lng);
+
+      document.getElementById("latitude").value = lat;
+      document.getElementById("longitude").value = lng;
+    });
+
+    $.ajax("/pins").then((pins) => {
+      console.log(map);
+      for (const pin of pins) {
+        const marker = new google.maps.Marker({
+          position: new google.maps.LatLng(pin.lat, pin.lng),
+          map: map,
+        });
+      }
+    });
   };
+}
 
+window.initMap = initMap;
